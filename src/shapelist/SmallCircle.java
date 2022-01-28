0811e8ae0b1a4374b6f3342a1c17ed2061670a02
navjit <kau10081@sheridancollege.ca>
2022-01-28 10:12:59 -0500

    my first commit

---
+++ src/shapelist/SmallCircle.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapelist;
+
+/**
+ *
+ * @author Navjit Kaur
+ */
+public class SmallCircle {
+    
+     private int radius;
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+}
